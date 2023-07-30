32f6eca90e730b276739526d4372888fdce19deb
vovapankiw <vovapankiw@gmail.com>
2023-07-30 10:18:12 +0300

    adedd popover tests

--- sandaes/src/pages/summary/SummaryForm.jsx
+++ sandaes/src/pages/summary/SummaryForm.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
       I agree to
-      <span style={{ color: "blue" }}> Terms and Conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
--- sandaes/src/pages/summary/test/SummaryForm.test.jsx
+++ sandaes/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "../SummaryForm";
 
@@ -20,13 +21,34 @@ describe("SummuryForm component", () => {
     expect(submitButton).toBeDisabled();
   });
 
-  it("should enable submit button by checkong the checkbox", () => {
+  it("should enable submit button by checkong the checkbox", async () => {
+    const user = userEvent.setup();
     render(<SummaryForm />);
 
     const checkbox = screen.getByRole("checkbox");
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     const submitButton = screen.getByRole("button", { name: "Confirm order" });
     expect(submitButton).toBeEnabled();
+  });
+
+  it("popover response to hover", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
+    const nullpopover = screen.queryByText(
+      /no ice cream will actully be delivered/i,
+    );
+    expect(nullpopover).not.toBeInTheDocument();
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i,
+    );
+    expect(popover).toBeInTheDocument();
+
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
   });
 });
