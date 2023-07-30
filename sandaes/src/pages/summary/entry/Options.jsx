7f21a0c7ebebe162a0d41e562883f5d24fd3b663
vovapankiw <vovapankiw@gmail.com>
2023-07-30 12:05:33 +0300

    added alert tests

--- sandaes/src/pages/summary/entry/Options.jsx
+++ sandaes/src/pages/summary/entry/Options.jsx
@@ -3,21 +3,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 
+import AlertBanner from "../../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToopingOptions from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
+      .catch(() => {
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToopingOptions;
