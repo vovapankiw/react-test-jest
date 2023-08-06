607d280e2fda836519c78a2fbc4de5dbf59c182c
vovapankiw <vovapankiw@gmail.com>
2023-08-06 13:39:34 +0300

    lint

--- sandaes/src/pages/entry/tests/totalUpdates.test.jsx
+++ sandaes/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
 import Options from "../Options";
 
 test("update scoop subtotal when scoops change", async () => {
